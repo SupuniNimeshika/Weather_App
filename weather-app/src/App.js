0790e81cfc7bcf096470669d3b7639c7786dfc0a
Your Name <supuni.nimeshika@gmail.com>
2018-03-15 16:50:23 +0530

    Successfully Get An Object

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -9,7 +9,11 @@ const API_KEY ="64177909d5005a429e0fe82724b7bd94";
 class App extends React.Component {
   getWeather = async (e) => {
     e.preventDefault();
-    const api_call =await fetch('url');
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call =await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${
+      API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data);
 
