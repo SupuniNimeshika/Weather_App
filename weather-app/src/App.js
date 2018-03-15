b50b5d4bd6421ea8b13e7e1bd1ec929217a9640d
Your Name <supuni.nimeshika@gmail.com>
2018-03-15 16:11:26 +0530

    Add url which contained Weather Data

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 
 import Titles from "./components/Titles";
 import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY ="64177909d5005a429e0fe82724b7bd94";
 
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call =await fetch('url');
+    const data = await api_call.json();
+    console.log(data);
+
+  }
+
   render() {
     return (
       <div>
         <Titles/>
         <Form/>
+        <Weather/>
       </div>
     );
   }
