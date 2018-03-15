184f7e701d16929ab13821785122e08cb413d7c5
Your Name <supuni.nimeshika@gmail.com>
2018-03-15 16:25:58 +0530

    Stop Refresh

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -7,7 +7,8 @@ import Weather from "./components/Weather";
 const API_KEY ="64177909d5005a429e0fe82724b7bd94";
 
 class App extends React.Component {
-  getWeather = async () => {
+  getWeather = async (e) => {
+    e.preventDefault();
     const api_call =await fetch('url');
     const data = await api_call.json();
     console.log(data);
